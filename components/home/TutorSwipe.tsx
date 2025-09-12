1deb90ed613c47b2b9a84fe5dfafeb5f3f356740
Nugo Bibilashvili <bibilashvili6@gmail.com>
2025-09-11 17:26:27 -0700

    iphone swipper

--- components/home/TutorSwipe.tsx
+++ components/home/TutorSwipe.tsx
@@ -7,119 +7,163 @@ import "swiper/css/navigation";
 import { tutorSwiper } from "../../constants/data";
 import Image from "next/image";
 import { ChevronLeft, ChevronRight, Star } from "react-coolicons";
+import { SectionHeader } from "../ui/Text";
 
 const TutorSwipe = () => {
   return (
-    <div className="w-full">
-      <Swiper
-        modules={[Navigation]}
-        spaceBetween={16}
-        slidesPerView={1.2}
-        breakpoints={{
-          400: {
-            slidesPerView: 1.5,
-          },
-          500: {
-            slidesPerView: 1.8,
-          },
-          640: {
-            slidesPerView: 2.4,
-          },
-          768: {
-            slidesPerView: 2.8,
-          },
-          868: {
-            slidesPerView: 3,
-          },
-          968: {
-            slidesPerView: 3.2,
-          },
-          1024: {
-            slidesPerView: 2,
-            spaceBetween: 24,
-          },
-          1124: {
-            slidesPerView: 2.2,
-            spaceBetween: 24,
-          },
-          1280: {
-            slidesPerView: 2.4,
-            spaceBetween: 24,
-          },
-          1380: {
-            slidesPerView: 2.6,
-            spaceBetween: 24,
-          },
-          1480: {
-            slidesPerView: 2.8,
-            spaceBetween: 24,
-          },
-          1536: {
-            slidesPerView: 2.9,
-            spaceBetween: 24,
-          },
-          1920: {
-            slidesPerView: 3,
-            spaceBetween: 24,
-          },
-        }}
-        navigation={{
-          nextEl: ".custom-next",
-          prevEl: ".custom-prev",
-        }}
-      >
-        {tutorSwiper?.map((item, index) => (
-          <SwiperSlide key={index} className="bg-[#F6F7FB] rounded-3xl p-3">
-            <div className="flex flex-col lg:flex-row items-center lg:gap-3">
-              <Image
-                src={item?.img}
-                alt={item?.fullName}
-                width={234}
-                height={144}
-                className="mx-auto rounded-2xl w-full max-h-[144px] lg:h-[320px] lg:w-auto lg:object-cover"
-              />
-              <div className="flex flex-col mt-3 p-4 rounded-2xl bg-white lg:mt-0">
-                <p className="text-[#737373] text-xs leading-4 font-helveticaneue-regular pb-1 lg:text-sm lg:leading-5">
-                  {item?.subject}
-                </p>
-                <span className="text-[#0C0F21] text-[28px] leading-[100%] font-lgvanastasia-regular pb-3 lg:text-[32px] lg:leading-5">
-                  {item?.fullName}
-                </span>
-                <p className="text-[#737373] text-xs leading-4 font-helveticaneue-regular pb-3 lg:pb-5">
-                  {item?.description}
-                </p>
-                <div className="flex gap-6">
-                  <div className="flex flex-col gap-1">
-                    <span className="text-[#0C0F21] text-2xl leading-6 font-spacegrotesk-bold">
-                      {item?.price}
-                    </span>
-                    <p className="text-[#737373] text-xs leading-4 font-helveticaneue-regular">
-                      ლარი საათში
-                    </p>
-                  </div>
-                  <div className="flex flex-col gap-1 pb-4 lg:pb-6">
-                    <div className="flex gap-2">
-                      <Star className="text-[#FF8C38] fill-[#FF8C38]" />
-                      <span className="text-base leading-6 font-spacegrotesk-bold">
-                        {item?.star}
+    <div
+      className="pt-[32px] sm:pt-[44px] lg:pt-[52px] xl:pt-[64px] 3xl:pt-[84px]
+    "
+    >
+      <div className="px-4 sm:px-6 lg:px-11 3xl:px-[160px] max-w-[1920px] 3xl:mx-auto">
+        <SectionHeader
+          title="ჩვენი გამოცდილი რეპეტიტორები"
+          description="ჩვენ გვჯერა, რომ ტექნოლოგიებს შეუძლიათ სწავლის გამოცდილების შეცვლა"
+        />
+      </div>
+
+      <div className="w-full max-w-[1920px] 3xl:mx-auto 3xl:px-10 overflow-hidden">
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={16}
+          slidesPerView={1.2}
+          slidesOffsetBefore={8}
+          slidesOffsetAfter={8}
+          breakpoints={{
+            400: {
+              slidesPerView: 1.5,
+              slidesOffsetBefore: 8,
+              slidesOffsetAfter: 8,
+            },
+            500: {
+              slidesPerView: 1.8,
+              slidesOffsetBefore: 8,
+              slidesOffsetAfter: 8,
+            },
+            640: {
+              slidesPerView: 2.4,
+              slidesOffsetBefore: 8,
+              slidesOffsetAfter: 8,
+            },
+            768: {
+              slidesPerView: 2.8,
+              slidesOffsetBefore: 8,
+              slidesOffsetAfter: 8,
+            },
+            868: {
+              slidesPerView: 3,
+              slidesOffsetBefore: 8,
+              slidesOffsetAfter: 8,
+            },
+            968: {
+              slidesPerView: 3.2,
+              slidesOffsetBefore: 8,
+              slidesOffsetAfter: 8,
+            },
+            1024: {
+              slidesPerView: 2,
+              spaceBetween: 24,
+              slidesOffsetBefore: 22,
+              slidesOffsetAfter: 22,
+            },
+            1124: {
+              slidesPerView: 2.2,
+              spaceBetween: 24,
+              slidesOffsetBefore: 22,
+              slidesOffsetAfter: 22,
+            },
+            1280: {
+              slidesPerView: 2.4,
+              spaceBetween: 24,
+              slidesOffsetBefore: 22,
+              slidesOffsetAfter: 22,
+            },
+            1380: {
+              slidesPerView: 2.6,
+              spaceBetween: 24,
+              slidesOffsetBefore: 22,
+              slidesOffsetAfter: 22,
+            },
+            1480: {
+              slidesPerView: 2.8,
+              spaceBetween: 24,
+              slidesOffsetBefore: 22,
+              slidesOffsetAfter: 22,
+            },
+            1536: {
+              slidesPerView: 2.9,
+              spaceBetween: 24,
+              slidesOffsetBefore: 40,
+              slidesOffsetAfter: 40,
+            },
+            1920: {
+              slidesPerView: 3,
+              spaceBetween: 24,
+              slidesOffsetBefore: 0,
+              slidesOffsetAfter: 0,
+            },
+          }}
+          navigation={{
+            nextEl: ".custom-next",
+            prevEl: ".custom-prev",
+          }}
+        >
+          {tutorSwiper?.map((item, index) => (
+            <SwiperSlide key={index} className="bg-[#F6F7FB] rounded-3xl p-3">
+              <div className="flex flex-col lg:flex-row items-center lg:gap-3">
+                <Image
+                  src={item?.img}
+                  alt={item?.fullName}
+                  width={234}
+                  height={144}
+                  className="mx-auto rounded-2xl w-full max-h-[144px] lg:h-[320px] lg:w-auto lg:object-cover"
+                />
+                <div className="flex flex-col mt-3 p-4 rounded-2xl bg-white lg:mt-0">
+                  <p className="text-[#737373] text-xs leading-4 font-helveticaneue-regular pb-1 lg:text-sm lg:leading-5">
+                    {item?.subject}
+                  </p>
+                  <span className="text-[#0C0F21] text-[28px] leading-[100%] font-lgvanastasia-regular pb-3 lg:text-[32px] lg:leading-5">
+                    {item?.fullName}
+                  </span>
+                  <p className="text-[#737373] text-xs leading-4 font-helveticaneue-regular pb-3 lg:pb-5">
+                    {item?.description}
+                  </p>
+                  <div className="flex gap-6">
+                    <div className="flex flex-col gap-1">
+                      <span className="text-[#0C0F21] text-2xl leading-6 font-spacegrotesk-bold">
+                        {item?.price}
                       </span>
+                      <p className="text-[#737373] text-xs leading-4 font-helveticaneue-regular">
+                        ლარი საათში
+                      </p>
                     </div>
-                    <p className="text-[#737373] text-xs leading-4 font-helveticaneue-regular">
-                      28 შეფასება
-                    </p>
+                    <div className="flex flex-col gap-1 pb-4 lg:pb-6">
+                      <div className="flex gap-2">
+                        <Star className="text-[#FF8C38] fill-[#FF8C38]" />
+                        <span className="text-base leading-6 font-spacegrotesk-bold">
+                          {item?.star}
+                        </span>
+                      </div>
+                      <p className="text-[#737373] text-xs leading-4 font-helveticaneue-regular">
+                        28 შეფასება
+                      </p>
+                    </div>
                   </div>
+                  <button className="text-[#FFFFFF] text-sm leading-5 font-helveticaneue-medium py-3 w-full bg-[#0C0F21FA]/98 rounded-[40px]">
+                    დეტალურად
+                  </button>
                 </div>
-                <button className="text-[#FFFFFF] text-sm leading-5 font-helveticaneue-medium py-3 w-full bg-[#0C0F21FA]/98 rounded-[40px]">
-                  დეტალურად
-                </button>
               </div>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
 
       {/* Custom navigation buttons */}
-      <div className="w-full flex items-center justify-center pt-6 lg:pt-8 sm:justify-between">
+      <div
+        className="w-full flex items-center justify-center pt-6 lg:pt-8 sm:justify-between
+      px-4 sm:px-6 lg:px-11 3xl:px-[160px] max-w-[1920px] 3xl:mx-auto"
+      >
         <div className="hidden sm:flex gap-3">
           <button className="custom-prev p-[10px] bg-[#111425] rounded-[40px] flex items-center justify-center">
             <ChevronLeft className="text-white" />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,35 @@
 import ChooseAnyTime from "../../components/home/ChooseAnyTime";
 import DiscoverEducation from "../../components/home/DiscoverEducation";
-import ExperiencedTutorsSection from "../../components/home/ExperiencedTutorsSection";
 import Hero from "../../components/home/Hero";
 import NavBar from "../../components/home/NavBar";
 import Tutors from "../../components/home/Tutors";
 import TutorsType from "../../components/home/TutorsType";
+import TutorSwipe from "../../components/home/TutorSwipe";
 import WhyOurPlatform from "../../components/home/WhyOurPlatform";
 import VideoLesson from "../../components/ui/VideoLesson";
 
 export default function Home() {
   return (
-    <div className="px-4 sm:px-6 lg:px-11 3xl:px-[160px] max-w-[1920px] 3xl:mx-auto">
-      <div className="hidden lg:block">
-        <NavBar />
-      </div>
-      <Hero />
-      <div className="lg:flex lg:gap-6">
-        <VideoLesson />
-        <div className="lg:order-3 lg:w-[20%] lg:h-full">
-          <Tutors />
-          <div className="hidden lg:block lg:w-full lg:h-full">
-            <TutorsType />
-          </div>
+    <>
+      <div className="px-4 sm:px-6 lg:px-11 3xl:px-[160px] max-w-[1920px] 3xl:mx-auto">
+        <div className="hidden lg:block">
+          <NavBar />
         </div>
-        <ChooseAnyTime />
+        <Hero />
+        <div className="lg:flex lg:gap-6">
+          <VideoLesson />
+          <div className="lg:order-3 lg:w-[20%] lg:h-full">
+            <Tutors />
+            <div className="hidden lg:block lg:w-full lg:h-full">
+              <TutorsType />
+            </div>
+          </div>
+          <ChooseAnyTime />
+        </div>
+        <DiscoverEducation />
+        <WhyOurPlatform />
       </div>
-      <DiscoverEducation />
-      <WhyOurPlatform />
-      <ExperiencedTutorsSection />
-    </div>
+      <TutorSwipe />
+    </>
   );
 }
