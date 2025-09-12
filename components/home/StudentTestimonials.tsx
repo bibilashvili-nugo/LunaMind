5342fe72dda06869f842bfa4a7ed292c01080000
Nugo Bibilashvili <bibilashvili6@gmail.com>
2025-09-12 07:23:49 -0700

    implement for all responsive comp what students say

--- components/home/StudentTestimonials.tsx
+++ components/home/StudentTestimonials.tsx
@@ -9,10 +9,17 @@ const StudentTestimonials = () => {
         title="რას ამბობენ ჩვენი სტუდენტები"
         description="ჩვენ გვჯერა, რომ ტექნოლოგიებს შეუძლიათ სწავლის გამოცდილების შეცვლა"
       />
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-col gap-4">
-          <div className="inline-flex flex-col bg-[#52CE91] text-center px-[26px] py-[20px] rounded-3xl gap-4 sm:items-center sm:gap-6 sm:p-[32px]">
-            <span className="font-lgvanastasia-regular text-[32px] leading-9 text-[#0C0F21] sm:w-[383px] sm:text-[52px] sm:leading-[52px]">
+      <div className="flex flex-col gap-4 lg:gap-6">
+        <div className="flex flex-col gap-4 lg:flex-row lg:gap-6">
+          <div
+            className="inline-flex flex-col bg-[#52CE91] text-center px-[26px] py-[20px] 
+            rounded-3xl gap-4 sm:items-center sm:gap-6 sm:p-[32px] lg:py-[32px] lg:px-[36px]
+          lg:w-1/2"
+          >
+            <span
+              className="font-lgvanastasia-regular text-[32px] leading-9 text-[#0C0F21] sm:w-[383px] 
+            sm:text-[52px] sm:leading-[52px]"
+            >
               გაიმყარე ცოდნა შეოუერთდი ჩვენს სტუდენტებს და დაიწყწე დღესვე
             </span>
             <div className="flex flex-col gap-3 items-center sm:flex-row">
@@ -26,7 +33,7 @@ const StudentTestimonials = () => {
           </div>
           <div
             className="inline-flex flex-col bg-[#F6F7FB] text-center p-5 items-center rounded-3xl gap-4 sm:p-[22px]
-          sm:flex-row sm:gap-8"
+          sm:flex-row sm:gap-8 lg:w-1/2"
           >
             <Image
               src={"/images/giorgi.png"}
@@ -35,8 +42,8 @@ const StudentTestimonials = () => {
               height={144}
               className="rounded-2xl"
             />
-            <div className="flex flex-col gap-4 sm:justify-between sm:h-[252px]">
-              <span className="text-sm leading-5 text-[#0C0F21] text-start font-helveticaneue-regular">
+            <div className="flex flex-col gap-4 sm:justify-between sm:h-[252px] 2xl:py-[20px]">
+              <span className="text-sm leading-5 text-[#0C0F21] text-start font-helveticaneue-regular xl:text-base xl:leading-6">
                 “ ცნობილი ფაქტია, რომ გვერდის წაკითხვად შიგთავსს შეუძლია
                 მკითხველის ყურადღება მიიზიდოს და დიზაინის აღქმაში ხელი შეუშალოს
                 “
@@ -47,8 +54,8 @@ const StudentTestimonials = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-4">
-          <div className="relative w-full h-[200px] sm:h-[296px] rounded-3xl overflow-hidden">
+        <div className="flex flex-col gap-4 lg:flex-row lg:gap-6">
+          <div className="relative w-full h-[200px] sm:h-[296px] rounded-3xl overflow-hidden lg:w-1/2 xl:h-[384px]">
             <Image
               src={"/images/meeting.png"}
               alt="meeting"
@@ -56,7 +63,10 @@ const StudentTestimonials = () => {
               className="object-cover"
             />
             <div className="absolute inset-0 bg-[#0C0F21]/50" />
-            <div className="absolute inset-0 flex flex-col items-end justify-end text-right text-white p-4 sm:p-8">
+            <div
+              className="absolute inset-0 flex flex-col items-end justify-end text-right text-white p-4 sm:p-8 lg:p-6
+            xl:py-8 2xl:px-[32px] 2xl:py-[44px] 3xl:px-[40px]"
+            >
               <span className="text-[44px] leading-[100%] font-spacegrotesk-bold sm:text-[54px]">
                 92%
               </span>
@@ -68,12 +78,12 @@ const StudentTestimonials = () => {
               </span>
             </div>
           </div>
-          <div className="flex flex-col gap-4 sm:flex-row ">
-            <div className="bg-[#7D3FFF] rounded-3xl p-6 sm:w-1/2">
+          <div className="flex flex-col gap-4 sm:flex-row lg:w-1/2 lg:gap-6">
+            <div className="bg-[#7D3FFF] rounded-3xl p-6 sm:w-1/2 lg:h-[296px] xl:h-[384px]">
               <div className="bg-white inline-flex p-[10px] rounded-full justify-center items-center ">
                 <Star />
               </div>
-              <div className="pt-[27px] sm:pt-[71px]">
+              <div className="pt-[27px] sm:pt-[71px] xl:pt-[160px]">
                 <div className="flex items-end">
                   <div>
                     <span className="text-[54px] leading-[100%] font-spacegrotesk-bold text-white">
@@ -91,8 +101,8 @@ const StudentTestimonials = () => {
                 </span>
               </div>
             </div>
-            <div className="bg-[#ECE3FF] rounded-3xl p-6 flex flex-col gap-[74px] sm:w-1/2 sm:h-[296px] sm:justify-between">
-              <span className="text-[#0C0F21] text-sm leading-5 font-helveticaneue-regular">
+            <div className="bg-[#ECE3FF] rounded-3xl p-6 flex flex-col gap-[74px] sm:w-1/2 sm:h-[296px] sm:justify-between xl:h-[384px]">
+              <span className="text-[#0C0F21] text-sm leading-5 font-helveticaneue-regular xl:text-base xl:leading-6">
                 “ ცნობილი ფაქტია, რომ გვერდის წაკითხვად შიგთავსს შეუძლია
                 მკითხველის ყურადღება მიიზიდოს და დიზაინის აღქმაში ხელი შეუშალოს
                 “
