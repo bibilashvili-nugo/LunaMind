acba18a4b6bc92e45e027569c11685f157e24d83
Nugo Bibilashvili <bibilashvili6@gmail.com>
2025-09-14 07:52:01 -0700

    implement footer icons

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -1,18 +1,40 @@
+import Image from "next/image";
+
 const Footer = () => {
   return (
     <div
       className="bg-[#080A16] rounded-tl-4xl rounded-tr-4xl mt-[55px] sm:mt-[44px] lg:mt-[64px] 3xl:mt-[84px]
-    flex flex-col gap-[44px]"
+      flex flex-col gap-[44px] relative overflow-hidden"
     >
-      <div className="pt-[44px] px-4 flex flex-col gap-3 lg:px-[44px] lg:pt-[64px] 2xl:px-[80px] 3xl:mt-[60px] 3xl:px-[160px]">
-        <span className="text-white text-[40px] leading-[40px] font-lgvanastasia-regular text-center lg:text-[64px] lg:leading-[64px]">
+      <div className="absolute w-[535px] h-[401px] sm:-right-80 md:-right-70 lg:-right-65 2xl:-right-60 3xl:-right-0 hidden sm:block z-0">
+        <Image src="/images/Burst.png" fill alt="burst" />
+      </div>
+      <div className="pt-[44px] px-4 flex flex-col gap-3 lg:px-[44px] lg:pt-[64px] 2xl:px-[80px] 3xl:mt-[60px] 3xl:px-[160px] relative z-10">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hidden sm:block z-9 w-[180px] h-[95px] lg:hidden ">
+          <Image
+            src="/images/SpiralCircle.png"
+            alt="circle"
+            width={180}
+            height={95}
+          />
+        </div>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hidden lg:block z-9 w-[254px] h-[135px]  ">
+          <Image
+            src="/images/SpiralCircle.png"
+            alt="circle"
+            width={254}
+            height={135}
+          />
+        </div>
+
+        <span className="text-white text-[40px] leading-[40px] font-lgvanastasia-regular text-center lg:text-[64px] lg:leading-[64px] relative z-10">
           მზად ხარ დაიწყო განათლების ინოვაცია დღესვე ?
         </span>
-        <p className="text-sm leading-5 font-helveticaneue-regular text-white/50 text-center lg:text-base lg:leading-6">
+        <p className="text-sm leading-5 font-helveticaneue-regular text-white/50 text-center lg:text-base lg:leading-6 relative z-10">
           შემოუერთდით ათასობით სტუდენტსა და რეპეტიტორს ჩვენს პლატფორმაზე
         </p>
       </div>
-      <div className="flex flex-col items-center gap-6 sm:flex-row sm:justify-center">
+      <div className="flex flex-col items-center gap-6 sm:flex-row sm:justify-center z-10">
         <span className="text-white text-[36px] leading-[100%] font-lgvanastasia-regular underline lg:text-[44px]">
           გახდი მოსწავლე
         </span>
@@ -20,7 +42,7 @@ const Footer = () => {
           გახდი რეპეტიტორი
         </span>
       </div>
-      <div className="px-4 pb-[32px] lg:px-[44px] lg:mt-[20px] 2xl:px-[80px] 3xl:px-[160px] 3xl:pt-[64px]">
+      <div className="px-4 pb-[32px] lg:px-[44px] lg:mt-[20px] 2xl:px-[80px] 3xl:px-[160px] 3xl:pt-[64px] z-10">
         <hr className="text-white/10 pb-6" />
         <div className="flex flex-col gap-5 lg:flex-row lg:justify-between lg:items-center">
           <span className="font-aclonica-regular text-white text-[32px] leading-[100%]">
