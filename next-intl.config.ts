2958facc1dd51b896159427f2922919aeab95bbe
Nugo Bibilashvili <bibilashvili6@gmail.com>
2025-09-23 07:47:45 -0700

    implement time zone

---
+++ next-intl.config.ts
@@ -0,0 +1,6 @@
+/** @type {import('next-intl').NextIntlConfig} */
+module.exports = {
+  locales: ["en", "ru", "ka"], // your supported locales
+  defaultLocale: "ka",
+  timeZone: "UTC", // global default timezone
+};
