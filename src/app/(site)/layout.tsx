4d58d0e3789c2d6341d6899096f73745fd333c75
Nugo Bibilashvili <bibilashvili6@gmail.com>
2025-12-03 04:56:29 -0800

    fix css

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,48 +1,41 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import Providers from "./Providers";
 import { ScrollToTop } from "@/utils/ScrollToTop";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
   subsets: ["latin"],
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
   title: "Evectus",
-  description:
-    "Evectus არის ონლაინ სასწავლო პლატფორმა, სადაც შეგიძლიათ აირჩიოთ პროფესიონალი რეპეტიტორი და ისწავლოთ სხვადასხვა საგნები: მათემატიკა, ქართული, ისტორია, გეოგრაფია, ქიმია, ფიზიკა, ბიოლოგია, ხელოვნება, ინგლისური, რუსული, გერმანული, ესპანური, ფრანგული, დაწყებითი კლასები. პლატფორმა საშუალებას გაძლევთ დაჯავშნოთ ინდივიდუალური გაკვეთილები, სწავლოთ ონლაინ სახლში ან ნებისმიერ სხვა ადგილას და გააუმჯობესოთ შედეგები სასწავლო პროცესში.",
-  icons: {
-    icon: "/favicon.ico",
-  },
+  description: "...",
+  icons: { icon: "/favicon.ico" },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={geistSans.className}>
+      <body className={`${geistMono.className} antialiased`}>
         <Providers>
           <ScrollToTop />
           {children}
           <Toaster
             position="top-right"
             reverseOrder={false}
-            toastOptions={{
-              className: "font-helveticaneue-medium",
-            }}
+            toastOptions={{ className: "font-helveticaneue-medium" }}
           />
         </Providers>
       </body>
